4d7d4886952fb167a5aec1410149806929e1de20
Jesus Rodriguez <Foxandxss@gmail.com>
2012-07-14 23:25:51 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foxandxss::Application.config.secret_token = '0133de6638ce64c6cd398f2defefda3e33efcf914e00df3bcbc84b186811d09bd1c1d8823db764275b79820773bd1ff3283313837faefd16dae973bf0f081a66'
